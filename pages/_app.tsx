a3f56ee45fac03fb08855d65df0e3a2da62363ca
3scava1i3r <tripathi.hritwik@gmail.com>
2023-07-01 15:57:19 +0530

    fix: github issues fixed

---
+++ pages/_app.tsx
@@ -0,0 +1,60 @@
+import '../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import type { AppProps } from 'next/app';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+  arbitrum,
+  goerli,
+  mainnet,
+  optimism,
+  polygon,
+  zora,
+} from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import { Header } from '../components/layout/header';
+import { Footer } from '../components/layout/footer';
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    zora,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+  ],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+    
+    <WagmiConfig config={wagmiConfig}>
+    
+      <RainbowKitProvider chains={chains}>
+      <Header />
+        <Component {...pageProps} />
+      {/* <Footer /> */}
+      </RainbowKitProvider>
+    </WagmiConfig>
+    </>
+    
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      
+      
+      <main className={styles.main}>
+<div>okok</div>
+        
+
+        
+      </main>
+
+      
+    </div>
+  );
+};
+
+export default Home;
